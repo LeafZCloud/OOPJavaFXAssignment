15711e37a9aba70f71ae430985a0dd05496bc15a
Alif Z <Zamaa@farmingdale.edu>
2026-03-01 20:51:55 -0500

    Oop JavaFX Assignment

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module edu.farmingdale.csc325_oopjavafxassignment {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens edu.farmingdale.csc325_oopjavafxassignment to javafx.fxml;
+    exports edu.farmingdale.csc325_oopjavafxassignment;
+}
